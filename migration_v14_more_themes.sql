3cd96a4e952b6973e789ed8310b2b1e747f27cfa
karambasaho24-eng <karambasaho24@gmail.com>
2026-06-29 23:31:49 +0200

    Delete migration_v14_more_themes.sql

--- migration_v14_more_themes.sql
+++
@@ -1,4 +0,0 @@
-alter table public.site_config drop constraint if exists site_config_theme_color_check;
-alter table public.site_config
-  add constraint site_config_theme_color_check
-  check (theme_color in ('red', 'green', 'blue', 'gold', 'purple', 'teal', 'orange', 'rose', 'slate', 'terracotta'));
